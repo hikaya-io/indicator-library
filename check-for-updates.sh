d8ab0463247a819cda9de733de74f64732efc436
Paul Spende <paul.spende@gmail.com>
2018-01-25 17:18:54 +0100

    auto-pull and deploy

---
+++ check-for-updates.sh
@@ -0,0 +1,2 @@
+[ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+> sed ‘s/\// /g’) | cut -f1) ] && echo up to date || ./deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 git pull -r
 
+sudo /usr/local/bin/docker-compose stop
 sudo /usr/local/bin/docker-compose run web django-admin.py startproject composeexample .
+sudo /usr/local/bin/docker-compose up -d
