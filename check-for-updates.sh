121637282704c72458fcc8e4e1e79e4b43b03c3f
Paul Spende <paul.spende@gmail.com>
2018-01-25 17:41:53 +0100

    fixed check

--- check-for-updates.sh
+++ check-for-updates.sh
@@ -1,4 +1,4 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
-> sed ‘s/\// /g’) | cut -f1) ] && echo up to date || $HOME/indicator-library/deploy.sh
+ sed 's/\// /g') | cut -f1) ] && echo up to date || $HOME/indicator-library/deploy.sh
