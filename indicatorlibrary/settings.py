c49f9c334860ff6f5c896d76869a9382d36cc34f
Paul Spende <paul.spende@gmail.com>
2018-02-02 17:14:24 +0100

    change db to postgres

--- indicatorlibrary/settings.py
+++ indicatorlibrary/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'indicatorlibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'indicator_library',
+        'USER': 'indlib',
+        'PASSWORD': 'password123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
